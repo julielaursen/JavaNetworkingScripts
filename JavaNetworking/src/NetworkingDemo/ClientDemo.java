0248c8ccd6a1f6fed9b386a4a3197aeed8b7a29a
Julie Laursen <julielaursen1@gmail.com>
2018-10-23 22:22:55 -0500

    Add files via upload
    Added in the functionality to do calculations

--- JavaNetworking/src/NetworkingDemo/ClientDemo.java
+++ JavaNetworking/src/NetworkingDemo/ClientDemo.java
@@ -1,7 +1,10 @@
 package NetworkingDemo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -11,46 +14,55 @@ public class ClientDemo {
 	
 	public static void main(String args[]) throws UnknownHostException, IOException
 	{
-		//refactor so client can enter all three numbers at once
 		int number1;
 		String quit;
-		//, number2, number3, \
-		int temp;
+		int number2, number3;
+		int sum;
 		int serverPort = 1000;
 		Scanner sc = new Scanner(System.in);
+		PrintWriter out = null;
+		BufferedReader in = null;
+		
 		Socket clientSocket = new Socket("127.0.0.1", serverPort);
+		//Socket connection = clientSocket.accept();
 		Scanner sc1 = new Scanner(clientSocket.getInputStream());
 		InetAddress serverIPAddress = InetAddress.getByName("localhost");
 		System.out.println("Attempting to send data to " +  serverIPAddress);
-		
+		System.out.println("This progam will calculate the sum of integers between two numbers.\nThe first number should be less than the second number");
 		System.out.println("Enter first number");
 		number1 = sc.nextInt();
-		//pass number to server
-		
-		PrintStream p = new PrintStream(clientSocket.getOutputStream());
-		
-		//System.out.println("Enter second number");
-		//number2 = sc.nextInt();
-		//System.out.println("Enter third number. Third number must be 1 or 2");
-		//number3 = sc.nextInt();
-		 
-		
-		p.println(number1);
-		//p.println(number2);
-		//p.println(number3);
-		
-		temp = sc1.nextInt();
-		System.out.println(temp);
-		
-		
-		System.out.println("Enter bye to indicate no more data");
-		quit = sc.nextLine();
+		PrintStream p1 = new PrintStream(clientSocket.getOutputStream());
+		System.out.println("Enter second number");
+		number2 = sc.nextInt();
 		PrintStream p2 = new PrintStream(clientSocket.getOutputStream());
-		if (quit == "Bye" || quit == "bye") {
-			p2.println(quit);
-			clientSocket.close();
-		}
+	    System.out.println("Enter 1 to sum all odd numbers between the two inputs. Enter 2 to sum all even numbers between the two inputs");
+		number3 = sc.nextInt();
+		PrintStream p3 = new PrintStream(clientSocket.getOutputStream());
+	
+		p1.println(number1);
+		p2.println(number2);
+		p3.println(number3);
+
+		//get sum back from server
+		//sum = sc1.nextInt();
+		//System.out.println(sum);
+
 		
+//		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+//		String userInput;
+//
+//		System.out.println("Type Message (\"Bye.\" to quit)");
+//		System.out.print("Input: ");
+//		while ((userInput = stdIn.readLine()) != null)
+//		{
+//			out.println(userInput);
+//			System.out.println("Client: " + in.readLine());
+//			// end loop if client sends "Bye."
+//			if (userInput.equals("Bye."))
+//				break;
+//
+//			System.out.print("Input: ");
+//		}
 	}
 
 }
--- JavaNetworking/src/NetworkingDemo/ServerDemo.java
+++ JavaNetworking/src/NetworkingDemo/ServerDemo.java
@@ -1,8 +1,12 @@
 package NetworkingDemo;
 
+import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -13,25 +17,76 @@ public class ServerDemo {
 	public static void main(String args[]) throws IOException
 	{  
 		int port = 1000;
-		int number, temp;
-		ServerSocket server = new ServerSocket(port, 100);
-	
+		int sum = 0;
+		int number;
+		int number2, number3;
 		
+		ServerSocket server = new ServerSocket(port, 100);		
 		System.out.println("Server Socket started on port " + port);
-		//listen for a client connection and return a socket when connection established
 		System.out.println("Waiting for connection");
 		Socket connection = server.accept();
 		//System.out.println("Connection received from " + connection.getInetAddress().getHostName());
 		Scanner sc = new Scanner(connection.getInputStream());
 		
 		number = sc.nextInt();
+		number2 = sc.nextInt();
+		number3 = sc.nextInt();
+		String clientMessage;
+		InputStream input = connection.getInputStream();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 		
-		temp = number * 2;
+		//do {
+		//clientMessage = reader.readLine();
+		if(number < number2) {
+		if(number3 % 2 == 0 ) {
+			System.out.println("Printing sum of even numbers");
+			if(number % 2 == 0){
+				for(int i = number; i <= number2; i+=2) {
+				sum = sum + i;}
+				}
+				else if(number %2 != 0) {
+				for(int i = number - 1; i <= number2; i+=2){
+				sum = sum + i;}
+				}
+			PrintStream p = new PrintStream(connection.getOutputStream());
+			System.out.println("The sum of the two numbers is " + sum);
+			}
 		
-		//server sends information to the client
-		PrintStream p = new PrintStream(connection.getOutputStream());
+		if(number3 % 2 != 0 ) {
+			System.out.println("Printing sum of odd numbers");
+			for(int i = number; i <= number2; i+=2)
+			{
+				sum = sum + i;
+			}
+			PrintStream p1 = new PrintStream(connection.getOutputStream());
+			System.out.println("The sum of the two numbers is " + sum);
+		}
+		}
+		else {
+			System.out.println("Program could not calculate. Number 1 must be less than number 2 for the program to calculate the difference.");
+		}
+		//} while (!clientMessage.equals("bye"));
+		PrintStream p2 = new PrintStream(connection.getOutputStream());
+		System.out.println("Calculation finished");
+
 		
-		p.println(temp);
+
+//		PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+//		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//		
+//		String inputLine;
+//		
+//		while ((inputLine = in.readLine()) != null) {
+//			System.out.println("Server: " + inputLine);
+//			out.println("From server " + inputLine);
+//			
+//			if (inputLine.equals("Bye."))
+//				break;
+//		}
+		//server.close();
+		//sc.close();
 	}
 
 }
+
+
